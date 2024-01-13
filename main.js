fc604046026224bc72120d494ad5dc28100db521
Djeisson schneider <you@example.com>
2024-01-13 09:21:41 -0300

    menu mobile

---
+++ main.js
@@ -0,0 +1,13 @@
+const menu = document.getElementsByid('links')
+
+function abrirMenu() {
+    if (links.style.display = 'none'){
+        links.style.display = 'flex'
+    } else {
+        links.style.display = 'none'
+    }
+}
+
+function fecharMenu() {
+    links.style.display = 'none'
+}
